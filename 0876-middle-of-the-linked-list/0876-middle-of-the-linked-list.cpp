8ea333aa7b0a4b4276cf9d36cffd3b8fd0229efc
Nihal Bambale <102454476+NihalBambale11@users.noreply.github.com>
2024-09-30 22:31:16 +0530

    Time: 4 ms (39.84%), Space: 9.4 MB (19.09%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -14,22 +14,18 @@ public:
        ListNode* temp = head;
         int size = 0;
 
-        // Step 1: Calculate the size of the list
         while (temp != nullptr) {
             size++;
             temp = temp->next;
         }
 
-        // Step 2: Find the middle index (second middle in case of even length)
         int middleIndex = size / 2;
 
-        // Step 3: Traverse to the middle node
         temp = head;
         for (int i = 0; i < middleIndex; i++) {
             temp = temp->next;
         }
 
-        // Return the middle node
         return temp;
         
     }
