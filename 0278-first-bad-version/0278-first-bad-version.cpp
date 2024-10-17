46563e3cbcd5bac272196aa17b25a627c1916f55
Nihal Bambale <102454476+NihalBambale11@users.noreply.github.com>
2024-10-17 19:47:13 +0530

    Time: 3 ms (23.99%), Space: 7.7 MB (12.57%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,12 +4,30 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-    for(int i = 1 ; i <= n ; i++){
-            if(isBadVersion(i)){
-                return i;
-            }
+//     for(int i = 1 ; i <= n ; i++){
+//             if(isBadVersion(i)){
+//                 return i;
+//             }
             
+//         }
+//         return -1;
+        
+        
+        int low = 1;
+        int mid;
+        int ans = 0;
+        int high = n;
+        while(low <= high){
+            mid = low + (high-low)/2;
+            if(isBadVersion(mid)){
+                ans = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
         }
-        return -1;
+        
+        return ans;
     }
 };
