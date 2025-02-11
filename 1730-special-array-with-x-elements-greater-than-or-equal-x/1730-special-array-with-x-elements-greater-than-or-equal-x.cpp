41996dcae7fad5600ed6ca23562bf4e00b841411
Nihal Bambale <102454476+NihalBambale11@users.noreply.github.com>
2025-02-11 23:25:22 +0530

    Time: 0 ms (100%), Space: 11.2 MB (66.33%) - LeetHub

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int i = 1 ;
+        int j = nums.size();
+
+        while(i <= j){
+            int mid = i + (j-i)/2;
+            int count = 0;
+            for(int k = 0 ;k < nums.size();k++){
+                if(nums[k] >= mid ){
+                    count++;
+                }
+            }
+
+            if(count == mid){
+                return mid;
+            }
+            else if(count < mid){
+                j = mid -1;
+            }
+            else{
+                i = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
