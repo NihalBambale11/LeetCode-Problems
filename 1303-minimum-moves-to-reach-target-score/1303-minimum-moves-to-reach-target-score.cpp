21367849636a2b8d110a3a7335f965e171d2ab23
Nihal Bambale <102454476+NihalBambale11@users.noreply.github.com>
2025-02-19 12:00:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (66.59%) - LeetHub

--- 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.cpp
+++ 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int minMoves(long long target, int maxDoubles) {
-        long long  count = 0;
-        if(maxDoubles == 0){
-            return target-1;
-        }
-        while(target != 1){
+        long long count = 0;
 
-            if(maxDoubles != 0 && (target % 2 == 0)){
-                target = target/2;
+        while (target != 1) {
+            if (maxDoubles == 0) {
+                return count+(target - 1);
+            }
+            if (maxDoubles != 0 && (target % 2 == 0)) {
+                target = target / 2;
                 maxDoubles--;
                 count++;
-            }
-            else{
+            } else {
                 target = target - 1;
                 count++;
             }
