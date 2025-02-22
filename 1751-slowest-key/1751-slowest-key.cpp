f59df750ec34a469533ffc5dedc2078bfb1b056b
Nihal Bambale <102454476+NihalBambale11@users.noreply.github.com>
2025-02-22 20:48:21 +0530

    Time: 15 ms (5.5%), Space: 34.9 MB (6.8%) - LeetHub

--- 1751-slowest-key/1751-slowest-key.cpp
+++ 1751-slowest-key/1751-slowest-key.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
-        int maxi = 0; 
-        int pressed = 0; 
-        char ch = 'a'; 
-
-        for(int i = 0; i < releaseTimes.size(); i++) {
-            int duration = releaseTimes[i] - pressed;
-            pressed = releaseTimes[i]; 
-            
-            if (duration > maxi || (duration == maxi && keysPressed[i] > ch)) {
-                maxi = duration;
-                ch = keysPressed[i];
-            }
+    char slowestKeyRecursion(vector<int>& releaseTimes, string keysPressed,int i,int maxi,char ch ,int pressed ){
+        //base case
+        if(i == releaseTimes.size() ){
+            return ch;
         }
-        return ch;
+
+        int dur = releaseTimes[i] - pressed;
+        
+
+        if(maxi < dur || (maxi == dur && keysPressed[i] > ch)){
+            maxi = dur;
+            ch = keysPressed[i];
+        } 
+
+        return slowestKeyRecursion(releaseTimes,keysPressed,i+1,maxi,ch,releaseTimes[i]);
+    }
+
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        char ch = 'a';
+        int i = 0;
+        int maxi = 0;
+        int pressed = 0;
+       return slowestKeyRecursion(releaseTimes,keysPressed,i,maxi,ch,pressed);
     }
 };
