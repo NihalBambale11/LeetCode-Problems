acd347e93cdea3b92589971c1267cd1a81c2be5b
Nihal Bambale <102454476+NihalBambale11@users.noreply.github.com>
2025-06-02 20:12:03 +0530

    Time: 3 ms (27.13%), Space: 23 MB (27.4%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> v (ratings.size() , 1);
+
+        for(int i = 1 ; i < ratings.size() ; i++){
+            if(ratings[i] > ratings[i-1]){
+                v[i] = v[i-1] + 1;
+            }
+        }
+
+        for(int i = ratings.size()-2 ; i >= 0 ;i--){
+            if(ratings[i] > ratings[i+1]){
+                v[i] = max(v[i],v[i+1]+1);
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < v.size() ; i++){
+            sum = sum + v[i];
+        }
+
+        return sum;
+    }
+};
