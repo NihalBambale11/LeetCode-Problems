8634049808c55c8cdf6f30ec3eec38f923f026c1
Nihal Bambale <102454476+NihalBambale11@users.noreply.github.com>
2025-06-08 19:14:13 +0530

    Time: 55 ms (24.77%), Space: 22.9 MB (32.11%) - LeetHub

---
+++ 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        stack<char> st;
+        int count = 0;
+        for(char x : directions){
+            if(!st.empty() && (st.top() == 'R' && x == 'L')  ){
+                count = count + 2;
+                st.pop();
+                st.push('S');
+            }
+            else if(!st.empty() && (st.top() == 'R' && x == 'S' )   ){
+                count = count + 1;
+                st.pop();
+                st.push('S');
+            }
+            else if(!st.empty() && (st.top() == 'S' && x == 'L')){
+                count = count + 1;
+
+            }
+            else{
+                st.push(x);
+            }
+        }
+
+        while (!st.empty() && st.top() == 'R') {
+            st.pop();
+        }
+        while (!st.empty()) {
+            if (st.top() == 'R') count++;
+            st.pop();
+        }
+
+
+
+        return count;
+    }
+};
